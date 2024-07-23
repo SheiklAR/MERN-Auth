051d881885d74c34926bc785f0ed58c286b2dd7f
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-07-23 18:08:10 +0530

    added toast contianer in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import Header from './components/Header'
-import { Outlet } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
+      <ToastContainer />
       <Container className='my-2'>
         <Outlet />
       </Container>
-      </div>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
